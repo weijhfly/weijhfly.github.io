aa944485500b468265567428ff0a7f52b57008a4
weijhfly <1241968795@qq.com>
2017-12-31 14:39:21 +0800

    Add files via upload

---
+++ js/my.js
@@ -0,0 +1,88 @@
+$.snowFall({
+	//创建粒子数量，密度
+	particleNo: 500,
+	//粒子下拉速度
+	particleSpeed:30,
+	//粒子在垂直（Y轴）方向运动范围
+	particleY_Range:1300,
+	//粒子在垂直（X轴）方向运动范围
+	particleX_Range:1000,
+	//是否绑定鼠标事件
+    bindMouse: false,
+    //相机离Z轴原点距离
+    zIndex:600,
+  //摄像机视野角度
+    angle:55,
+    wind_weight:0
+});
+var interval,bg = ['bg','bg2'],next = 0,btn;
+
+interval=function() {
+	if($('#pic-btn').hasClass('opacity4')){
+	   $('#pic-btn').attr('class','opacity9');
+	}else{
+	   $('#pic-btn').attr('class','opacity4');
+	}
+	
+}
+
+btn = setInterval(interval,5000);
+ next = 1;
+ 
+$('#pic-btn').bind('click',function(){
+	next==2 && (next=0);
+	$('body').attr('class',bg[next]);
+	next++;
+	$('.text-box').fadeOut(1000);
+	$('body').attr('class') == 'bg2'&&($('.text-box').eq(0).fadeIn(1000));
+})
+
+$('#pic-btn').mouseenter(function(){
+	$('#pic-btn').attr('class','opacity9');
+	window.clearInterval(btn);
+}).mouseleave(function(){
+	$('#pic-btn').attr('class','opacity4');
+	btn = setInterval(interval,5000);
+})
+window.onload = function(){
+	var images = new Array()
+	function preload() {
+
+	    for (i = 0; i < preload.arguments.length; i++) {
+
+	        images[i] = new Image()
+
+	        images[i].src = preload.arguments[i]
+
+	    }
+
+	}
+	if($(window).width() <= 768){
+	 preload(
+	'./images/mobile_snow02.jpg'
+	)      
+	}else{
+	preload(
+	'./images/snow02.jpg'
+	)      
+	}
+
+	$('#audio').bind('click',function(){
+		bf();
+	})
+	var audio = document.getElementById('myAudio'); 
+	audio.play();
+	function bf(){
+	 if(audio!==null){             
+	  if(audio.paused){                 
+	      audio.play();//audio.play();
+	      $('#audio').removeClass('stop');
+	  }else{
+	   audio.pause();// 这个就是暂停
+	     $('#audio').addClass('stop');
+	  }
+ 	} 
+}
+}
+setTimeout(function() {$('#pic-btn span').fadeIn();}, 1000);
+setTimeout(function() {$('#pic-btn span').fadeOut();}, 4000);
