cd437e44dcda9600e6e5323e3b9ea479bfdcb9a7
weijhfly <1241968795@qq.com>
2018-01-18 15:37:52 +0800

    Update tap.js

--- js/tap.js
+++ js/tap.js
@@ -1,84 +1,77 @@
 /*!
  * tap.js
- * by weijianhua  https://github.com/tap
+ * by weijianhua  https://github.com/weijhfly/tap
 */
 ;(function (factory) {
 	if (typeof define === 'function' && define.amd) {
-		define(factory);
+		define(function(){return factory;});
+	}else if (typeof exports == "object") {
+		module.exports = factory;
 	}else {
-		factory();
+		window.tap = factory;
 	}
-}(function () {
-	window.tap = function(){
-		var arg = arguments,
-			els = document.querySelectorAll(arg[0]),
-			isTouch = "ontouchend" in document,
-			len = els.length,
-			i = 0,
-			isEntrust = arg.length == 3;
+}(function(){
+	var arg = arguments,
+		els = document.querySelectorAll(arg[0]),
+		isTouch = "ontouchend" in document,
+		len = els.length,
+		i = 0,
+		isEntrust = typeof arg[1]== 'string';
 
-		if(len == 0){return false;}
-		while (i < len){
-			if(isTouch){
-				var o = {};
-				els[i].addEventListener('touchstart',function(e){
-					e.preventDefault();
-					var t = e.touches[0];
-					o.startX = t.clientX;
-		            o.startY = t.clientY;
-		            o.endX = t.clientX;
-		            o.endY = t.clientY;
-		            o.sTime = + new Date;
-				});
-				els[i].addEventListener('touchmove',function(e){
-					var t = e.touches[0];
-					o.endX = t.clientX;
-		            o.endY = t.clientY;
-				});
-				els[i].addEventListener('touchend',function(e){
-					var t = e.touches[0];
-		            if((+ new Date)-o.sTime<300){
-		                if(Math.abs(o.endX-o.startX)+Math.abs(o.endY-o.startY)<20){
-		                    e.preventDefault();
-		                    if(isEntrust){
-								if(equal(e,arg[1])){
-									arg[2].call(e.target);
-								}
-							}else{
-								arg[1].call(this);
+	if(len == 0){return false;}
+	while (i < len){
+		if(isTouch){
+			var o = {};
+			els[i].addEventListener('touchstart',function(e){
+				var t = e.touches[0];
+				o.startX = t.clientX;
+				o.startY = t.clientY;
+				o.sTime = + new Date;
+				if(arg[isEntrust ? 3:2]){e.stopPropagation();}
+			});
+			els[i].addEventListener('touchend',function(e){
+				var t = e.changedTouches[0];
+				o.endX = t.clientX;
+				o.endY = t.clientY;
+				if((+ new Date)-o.sTime<300){
+					if(Math.abs(o.endX-o.startX)+Math.abs(o.endY-o.startY)<20){
+						e.preventDefault();
+						if(isEntrust){
+							if(equal(e,arg[1])){
+								arg[2].call(e.target);
 							}
-		                }
-		            }
-		            o.sTime = undefined;
-		            o.startX = undefined;
-		            o.startY = undefined;
-		            o.endX = undefined;
-		            o.endY = undefined;
-		        });
-			}else{
-				els[i].addEventListener('click',function(e){
-					if(isEntrust){
-						if(equal(e,arg[1])){
-							arg[2].call(e.target);
+						}else{
+							arg[1].call(this);
 						}
-					}else{
-						arg[1].call(this);
 					}
-				});
-			}
-		 	i ++;
+				}
+				if(arg[isEntrust ? 3:2]){e.stopPropagation();}
+				o = {};
+			});
+		}else{
+			els[i].addEventListener('click',function(e){
+				if(arg[isEntrust ? 3:2]){e.stopPropagation();}
+				if(isEntrust){
+					if(equal(e,arg[1])){
+						arg[2].call(e.target);
+					}
+				}else{
+					arg[1].call(this);
+				}
+			});
 		}
+		i ++;
+	}
 
-		function equal(e,el){
-			var flag = false;
-			if(el.indexOf('.') != -1 && e.target.className == el.replace('.','')){
-				flag = true;
-			}else if(el.indexOf('#') != -1 && e.target.id == el.replace('#','')){
-				flag = true;
-			}else if(e.target.nodeName.toLocaleLowerCase() == el){
-				flag = true;
-			}
-			return flag;
+	function equal(e,el){
+		var flag = false;
+		if(el.indexOf('.') != -1 && e.target.className == el.replace('.','')){
+			flag = true;
+		}else if(el.indexOf('#') != -1 && e.target.id == el.replace('#','')){
+			flag = true;
+		}else if(e.target.nodeName.toLocaleLowerCase() == el){
+			flag = true;
 		}
+		return flag;
 	}
 }))
