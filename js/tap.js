b0ab26c6fde5cb967822a0be5ef2af28ff105ba2
weijhfly <1241968795@qq.com>
2018-01-27 14:12:25 +0800

    Update tap.js

--- js/tap.js
+++ js/tap.js
@@ -1,5 +1,5 @@
 /*!
- * tap.js v1.2.0
+ * tap.js v1.2.1
  * by weijianhua  https://github.com/weijhfly/tap
 */
 ;(function (factory) {
@@ -12,9 +12,9 @@
 	}
 }(function(){
 	var arg = arguments,
-		doc = document,
-		els = arg[0] == doc ? [doc]:document.querySelectorAll(arg[0]),
-		isTouch = "ontouchend" in document,
+		doc = window.document,
+		els = arg[0] == doc ? [doc]:doc.querySelectorAll(arg[0]),
+		isTouch = "ontouchend" in doc,
 		len = els.length,
 		i = 0,
 		isEntrust = typeof arg[1]== 'string',
@@ -31,16 +31,16 @@
 				o.sTime = + new Date;
 			});
 			els[i].addEventListener('touchend',function(e){
-				var tagName = e.target.tagName.toLocaleLowerCase();
-				if(tagName != 'select' && tagName != 'input' && tagName != 'textarea'){
-					document.activeElement.blur();
-					e.preventDefault();
-				}
 				var t = e.changedTouches[0];
 				o.endX = t.pageX;
 				o.endY = t.pageY;
 				if((+ new Date)-o.sTime<300){
 					if(Math.abs(o.endX-o.startX)+Math.abs(o.endY-o.startY)<20){
+						var tagName = e.target.tagName.toLocaleLowerCase();
+						if(tagName != 'select' && tagName != 'input' && tagName != 'textarea'){
+							doc.activeElement.blur();
+							e.preventDefault();
+						}
 						handler(e,arg,this);
 					}
 				}
